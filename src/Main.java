93d37f89e1e624ab13e9505fcefd5b308ab49462
shenuekanayake <shyaminadishani112000@gmail.com>
2023-08-15 20:56:41 +0530

    create a object for a DeluxPizza class and run all methods using that object.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-       Pizza p=new Pizza(false);
-       p.addExCheese();
-       p.addExTopping();
-       p.pack();
-       p.getBill();       //670
+        Pizza p=new Pizza(true);
+        p.addExCheese();
+        p.addExTopping();
+        p.pack();
+        p.getBill();
+
+        DeluxPizza dp=new DeluxPizza(true);
+        dp.getBill();
     }
 }
